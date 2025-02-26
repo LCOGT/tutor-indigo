6810b63fdb190698489434a09bf7c73a61dbaf98
Edward Gomez <edward@gomez.me.uk>
2025-02-26 11:27:06 +0000

    Fix indentation

--- tutorindigo/plugin.py
+++ tutorindigo/plugin.py
@@ -143,12 +143,13 @@ hooks.Filters.ENV_PATCHES.add_item(
 hooks.Filters.ENV_PATCHES.add_items(
     [
 # Stop email activation because emails are not working
-       ( "openedx-common-settings",
+       ( 
+           "openedx-common-settings",
         """
-        FEATURE['SKIP_EMAIL_VALIDATION'] = True,
-        FEATURES['ALWAYS_REDIRECT_HOMEPAGE_TO_DASHBOARD_FOR_AUTHENTICATED_USER'] = False,
-        FEATURES['ENABLE_THIRD_PARTY_AUTH'] = False,
-        """,
+FEATURE['SKIP_EMAIL_VALIDATION'] = True,
+FEATURES['ALWAYS_REDIRECT_HOMEPAGE_TO_DASHBOARD_FOR_AUTHENTICATED_USER'] = False,
+FEATURES['ENABLE_THIRD_PARTY_AUTH'] = False,
+""",
        ),
         # for production
         (
